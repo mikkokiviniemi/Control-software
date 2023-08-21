5ff4fb4eab0ee479aedb3864978d0823c1f8172e
An-Man <anna.pikkarainen@gmail.com>
2023-08-21 15:30:07 +0300

    init on this branch

---
+++ include/json_output.hpp
@@ -0,0 +1,65 @@
+#ifndef JSON_OUTPUT_H
+#define JSON_OUTPUT_H
+#include "external/json.hpp"
+#include "external/json_fwd.hpp"
+#include <array>
+
+using json = nlohmann::json;
+
+std::array<float, 16> temp_sensors; // tämä arrayksi voi olla mahdollinen
+
+json output_data = {
+    {"speed_of_conveyor", 0}, // uint16_t 
+    {"heater_1", false},
+    {"heater_2", false},
+    {"heater_3", false},
+    {"cooler", false},
+    {"qc_camera_status", false},
+    {"temp_sensors", {0.0f, 0.0f, 0.0f}}, // tai sitten näin mutta 16 kpl
+    //...
+};
+
+/*
+JSON Output:
+{
+    "speed_of_conveyor" : int,
+    "heater_1": bool,
+    "heater_2": bool,
+    "heater_3": bool,
+    "cooler": bool,
+    "qc_camera_status": bool,
+    "temp_sensor01": float,
+    "temp_sensor02": float,
+    "temp_sensor03": float,
+    "temp_sensor04": float,
+    "temp_sensor05": float,
+    "temp_sensor06": float,
+    "temp_sensor07": float,
+    "temp_sensor08": float,
+    "temp_sensor09": float,
+    "temp_sensor10": float,
+    "time_stamp": value
+}
+
+/*
+JSON Output_2:
+
+{
+    "qc_camera_fails": ???,
+    "time_stamp": value
+}
+*/
+
+/*
+JSON Commands:
+{
+    "speed_of_conveyor" : int,
+    "heater_1": bool,
+    "heater_2": bool,
+    "heater_3": bool,
+    "cooler": bool,
+    "qc_camera_status": bool,
+}
+*/
+
+#endif
--- main.cpp
+++ main.cpp
@@ -27,49 +27,6 @@ struct control_data {
     uint8_t camera_toggle;
 };
 
-/*
-JSON Output:
-{
-    "speed_of_conveyor" : int,
-    "heater_1": bool,
-    "heater_2": bool,
-    "heater_3": bool,
-    "cooler": bool,
-    "qc_camera_status": bool,
-    "temp_sensor01": float,
-    "temp_sensor02": float,
-    "temp_sensor03": float,
-    "temp_sensor04": float,
-    "temp_sensor05": float,
-    "temp_sensor06": float,
-    "temp_sensor07": float,
-    "temp_sensor08": float,
-    "temp_sensor09": float,
-    "temp_sensor10": float,
-    "time_stamp": value
-}
-*/
-/*
-JSON Output_2:
-
-{
-    "qc_camera_fails": ???,
-    "time_stamp": value
-}
-*/
-
-/*
-JSON Commands:
-{
-    "speed_of_conveyor" : int,
-    "heater_1": bool,
-    "heater_2": bool,
-    "heater_3": bool,
-    "cooler": bool,
-    "qc_camera_status": bool,
-}
-*/
-
 int main()
 {
     bool systems_online{ true };
