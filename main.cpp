721521fd2241134132b25f05a2369e0368d8f6f2
mikkoki <mikko.kiviniemi@brightstraining.com>
2023-08-22 13:09:21 +0300

    Add json_to_control data function

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <bitset>
+#include "external/json.hpp"
 
 struct sensor_data {
     uint8_t speed_of_conveyor;
@@ -36,6 +38,24 @@ std::vector<sensor_data> dummy_data_generator(int amount) {
         dummy_data.push_back(data_block);
     }
     return dummy_data;
+}
+
+//Convert Json to control_data
+control_data json_to_control_data(const nlohmann::json& json_elem) {
+    control_data new_data;
+    new_data.speed_of_conveyor = json_elem["speed_of_conveyor"];
+    bool heater_1 = json_elem["heater_1"];
+    bool heater_2 = json_elem["heater_2"];
+    bool heater_3 = json_elem["heater_3"];
+    bool cooler = json_elem["cooler"];
+    new_data.heaters_cooler = (heater_1 << 0) | (heater_2 << 1) | (heater_3 << 2) | (cooler << 3);
+    new_data.camera_toggle = json_elem["qc_camera_status"];
+    return new_data;
+}
+//Function to display bits
+void display_bits(uint8_t value) {
+    std::bitset<8> bits(value);
+    std::cout << bits << "\n";
 }
 
 /*
@@ -115,8 +135,30 @@ int main()
             //log_data()    
         }
 
+        // Test JSON data
+        std::string json_string = R"(
+            {
+                "speed_of_conveyor": 5,
+                "heater_1": true,
+                "heater_2": true,
+                "heater_3": true,
+                "cooler": false,
+                "qc_camera_status": true
+            }
+        )";
 
-        control_data data = ui_loop(systems_online);
+        nlohmann::json json_data = nlohmann::json::parse(json_string);
+        control_data data = json_to_control_data(json_data);
+
+        std::cout << "Speed of Conveyor: ";
+        std::cout << int(data.speed_of_conveyor) << "\n";
+        std::cout << "Heaters and Cooler: ";
+        display_bits(data.heaters_cooler);
+        std::cout << "Camera Toggle: ";
+        display_bits(data.camera_toggle);
+
+
+        data = ui_loop(systems_online);
 
     }
 
