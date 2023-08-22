55cddf4af243119853ef3448f1f1eed6c4d6b3b7
An-Man <anna.pikkarainen@gmail.com>
2023-08-22 14:33:34 +0300

    revert main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,85 @@
 #include "text_ui.hpp"
-#include "json_output.hpp"
 
 #include <iostream>
+#include <vector>
 #include <chrono>
 
+struct sensor_data {
+    uint8_t speed_of_conveyor;
+    uint16_t qc_camera_fails;
+
+    int16_t temp_sensor01;
+    int16_t temp_sensor02;
+    int16_t temp_sensor03;
+    int16_t temp_sensor04;
+    int16_t temp_sensor05;
+    int16_t temp_sensor06;
+    int16_t temp_sensor07;
+    int16_t temp_sensor08;
+    int16_t temp_sensor09;
+    int16_t temp_sensor10;
+
+    std::time_t time_stamp;
+};
+/*
+struct control_data {
+    uint8_t speed_of_conveyor;
+    uint8_t heaters_cooler;
+    uint8_t camera_toggle;
+};*/
+
+//Simple data_generator
+std::vector<sensor_data> dummy_data_generator(int amount) {
+    std::vector<sensor_data> dummy_data;
+    for (int i = 0; i < amount; i++) {
+        sensor_data data_block = {1,1,1,1,1,1,1,1,1,1,1,1,std::time(nullptr)};
+        dummy_data.push_back(data_block);
+    }
+    return dummy_data;
+}
+
+/*
+JSON Output:
+{
+    "speed_of_conveyor" : int,
+    "heater_1": bool,
+    "heater_2": bool,
+    "heater_3": bool,
+    "cooler": bool,
+    "qc_camera_status": bool,
+    "temp_sensor01": float,
+    "temp_sensor02": float,
+    "temp_sensor03": float,
+    "temp_sensor04": float,
+    "temp_sensor05": float,
+    "temp_sensor06": float,
+    "temp_sensor07": float,
+    "temp_sensor08": float,
+    "temp_sensor09": float,
+    "temp_sensor10": float,
+    "time_stamp": value
+}
+*/
+/*
+JSON Output_2:
+
+{
+    "qc_camera_fails": ???,
+    "time_stamp": value
+}
+*/
+
+/*
+JSON Commands:
+{
+    "speed_of_conveyor" : int,
+    "heater_1": bool,
+    "heater_2": bool,
+    "heater_3": bool,
+    "cooler": bool,
+    "qc_camera_status": bool,
+}
+*/
 
 int main()
 {
@@ -18,7 +94,7 @@ int main()
     bool qc_camera{ false };
     */
     
-    /*
+
     while (systems_online)
     {
         //read_sensor_data()
@@ -40,11 +116,9 @@ int main()
         }
 
 
-        ui_loop(systems_online, speed_of_conveyor, heaters_cooler, camera_toggle);
+        control_data data = ui_loop(systems_online);
 
     }
-    */
-
 
     return 0;
 }
