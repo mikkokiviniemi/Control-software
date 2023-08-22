d0620738c89050faa062f409419f5a0992434142
mikkoki <mikko.kiviniemi@brightstraining.com>
2023-08-22 09:48:57 +0300

    Modify text_ui
    -Small modifications for text ui

--- include/text_ui.hpp
+++ include/text_ui.hpp
@@ -3,6 +3,14 @@
 
 #include <stdint.h>
 
-void ui_loop(bool &display_ui, uint8_t &speed_of_conveyor, uint8_t &heaters_cooler, uint8_t &camera_toggle);
+//Control data structure
+struct control_data {
+    uint8_t speed_of_conveyor;
+    uint8_t heaters_cooler;
+    uint8_t camera_toggle;
+};
+
+control_data ui_loop(bool &display_ui);
 
 #endif
+
