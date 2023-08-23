f4ddd93df965331d87401f3131bc682409bded03
Oskar Siegfrids <oskar.siegfrids@brightstraining.com>
2023-08-23 22:31:04 +0300

    simulation_shm_wrapper done
    - Initial version done

--- include/shared_memory_wrapper.cpp
+++ include/shared_memory_wrapper.cpp
@@ -33,19 +33,18 @@ static int get_shared_block(const std::string& filename, std::size_t size)
     return shmget(key, size, 0666 | IPC_CREAT);
 }
 
-char* attach_memory_block(const std::string& filename, std::size_t size)
+uint8_t* attach_memory_block(const std::string& filename, std::size_t size)
 {
     //Get the block-id for the specific file
     int shared_block_id{ get_shared_block(filename, size) };
 
-    char* result;
     if (shared_block_id == SHM_ERROR)
     {
         return nullptr;
     }
 
     //map the shared block into this process's memory and give a pointer to it
-    result = shmat(shared_block_id, nulltpr, 0);
+    uint8_t* result = shmat(shared_block_id, nullptr, 0);
     if (result == reinterpret_cast<char*>(SHM_ERROR))
     {
         return nullptr;
@@ -55,12 +54,12 @@ char* attach_memory_block(const std::string& filename, std::size_t size)
 }
 
 //Detach the memory block from process, and return boolean if succesful or not
-bool detach_memory_block(char* block)
+bool detach_memory_block(uint8_t* block)
 {
     return (shmdt(block) != SHM_ERROR);
 }
 
-bool destroy_memory_block(char* filename)
+bool destroy_memory_block(uint8_t* filename)
 {
     int shared_block_id{ get_shared_block(filename, 0) };
 
@@ -73,16 +72,60 @@ bool destroy_memory_block(char* filename)
     return (shmctl(shared_block_id, IPC_RMID, nullptr) != SHM_ERROR);
 }
 
-class shm_wrapper
+class simulation_shm_wrapper
 {
 public:
-    shm_wrapper(const std::string& filename, std::size_t block_size = 4096);
+    simulation_shm_wrapper(const std::string& new_filename, std::size_t new_block_size = 4096)
+        : filename{ new_filename }, block_size{ new_block_size }, ptr_to_memory{attach_memory_block(filename, block_size)}
+    {}
     
+    int16_t read_temperature_sensor(uint8_t index)
+    {
+        return (static_cast<int16_t>(*(ptr_to_memory + index * 2)) << 8) | *(ptr_to_memory + index * 2 + 1);
+    }
+    uint8_t read_conveyor_speed_sensor()
+    {
+        return *(ptr_to_memory + 20);
+    }
+    uint16_t read_qc_camera_feed();
+    {
+        return (static_cast<int16_t>(*(ptr_to_memory + 25)) << 8) | *(ptr_to_memory + 26);
+    }
+    void set_conveyor_target_speed(uint8_t target_speed)
+    {
+        *(ptr_to_memory + 21) = target_speed;
+    }
+    
+    void set_heaters(uint8_t heaters)
+    {
+        *(ptr_to_memory + 22) = heaters;
+    }
+    void set_cooler(uint8_t cooler)
+    {
+        *(ptr_to_memory + 23) = cooler;
+    }
+    void set_camera_status(uint8_t camera_status)
+    {
+        *(ptr_to_memory + 24) = camera_status;
+    }
+    
+    ~simulation_shm_wrapper()
+    {
+        if (detach_memory_block)
+        {
+            std::cout << filename << " memory_block detached succesfully\n";
+        }
+        else
+            std::cout << filename << " memory_block detachment unsuccesfull\n";
+    }
+
+
 private:
     std::string filename;
-    std::size_t mem_size;
+    std::size_t block_size;
     char* ptr_to_memory;
 
 };
+
 
 #endif
