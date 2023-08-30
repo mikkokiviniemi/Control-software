ab74b05b7004dfce0db3bb72606e7d1a53009b8e
mikkoki <mikko.kiviniemi@brightstraining.com>
2023-08-30 10:55:35 +0300

    Testing automation
    - Small unit testing for automation

--- include/automatic_controls.hpp
+++ include/automatic_controls.hpp
@@ -4,7 +4,6 @@
 #include "data_structures.hpp"
 #include "../external/json.hpp"
 #include <iostream>
-#include <iostream>
 #include <bitset>
 #include <chrono>
 
@@ -32,7 +31,9 @@ int16_t mean_temp(const sensor_data &data);
 
 uint8_t conveyor_control(const sensor_data& data);
 
-uint8_t cooling_control(const sensor_data& data);
+uint8_t heating_control(const sensor_data &data, uint8_t heater);
+
+uint8_t cooling_control(const sensor_data &data);
 
 void automatic_loop(const sensor_data &sens_data, control_data& ctrl_data, const json& control_json,const bool& heater_period,const bool& start_period);
 
